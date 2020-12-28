bf1a2d8cd45cb0a671c995509ab88c2760053b40
Matthew Holmes <matt_holmes123@hotmail.com>
2020-12-28 18:05:09 +0000

    Add playground router & CORS middleware for local development.

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from routers import playground
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:8081",
+]
+
+# Read this for more information:
+# https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+# https://fastapi.tiangolo.com/tutorial/cors/?h=+access+control+allow+origin
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# This isn't a real router, its just to get used to fast api
+app.include_router(playground.router)
 
 
 @app.get("/")
