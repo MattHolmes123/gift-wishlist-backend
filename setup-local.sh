085492df624eb54f09ecc6b81516887e03fe201a
Matthew Holmes <matt_holmes123@hotmail.com>
2020-12-31 07:50:15 +0000

    Add alembic and sqlalchemy for database access and migration.
    Added simple model to test migration tool.

--- setup-local.sh
+++ setup-local.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 echo "Installing project dependencies (including dev dependencies)"
+# https://python-poetry.org/docs/configuration/#virtualenvsin-project-boolean
+poetry config virtualenvs.in-project true --local
 poetry install
---
+++ wishlist/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+
+from database.db import Base
+
+
+class WishListItem(Base):
+    __tablename__ = "wishlist_item"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    url = Column(String, nullable=False)
