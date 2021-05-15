122ee5192ae2a60e98aa23ea12972054485e2099
Matthew Holmes <matt_holmes123@hotmail.com>
2021-05-15 10:08:56 +0100

    Add admin endpoint to fetch wishlist for a given user id.

--- app/api/api.py
+++ app/api/api.py
@@ -8,4 +8,5 @@ api_router = APIRouter(prefix=settings.api_v1_str)
 
 api_router.include_router(login.router)
 api_router.include_router(users.router)
+api_router.include_router(wishlist.admin_router)
 api_router.include_router(wishlist.router)
